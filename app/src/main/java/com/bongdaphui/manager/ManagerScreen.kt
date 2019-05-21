3e15177f8d2a73b1806b68dac26d4fff96230b0d
Nguyen Tan Linh <nguyen.tan.linh@tee-coin.com>
2019-05-21 22:19:13 +0700

    [ADD] add expand club manager

--- app/src/main/java/com/bongdaphui/manager/ManagerScreen.kt
+++ app/src/main/java/com/bongdaphui/manager/ManagerScreen.kt
@@ -10,12 +10,14 @@ import com.bongdaphui.base.BaseFragment
 import com.bongdaphui.dialog.AlertDialog
 import com.bongdaphui.listener.ConfirmListener
 import com.bongdaphui.login.LoginScreen
-import com.bongdaphui.schedulePlayer.SchedulePlayerScreen
 import com.bongdaphui.model.UserModel
 import com.bongdaphui.myClub.MyClubScreen
 import com.bongdaphui.profile.ProfileScreen
 import com.bongdaphui.scheduleClub.ScheduleClubScreen
+import com.bongdaphui.schedulePlayer.SchedulePlayerScreen
 import com.bongdaphui.utils.Enum
+import com.bongdaphui.utils.Tools
+import com.bongdaphui.utils.ViewAnimation
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.frg_manager.*
@@ -86,6 +88,11 @@ class ManagerScreen : BaseFragment() {
             addFragment(ProfileScreen.getInstance(""))
         }
 
+        frg_manager_bt_toggle_info.setOnClickListener {
+
+            toggleSection(it, frg_manager_expand_info)
+        }
+
         frg_manager_my_club_list.setOnClickListener {
             addFragment(MyClubScreen())
         }
@@ -116,6 +123,29 @@ class ManagerScreen : BaseFragment() {
         frg_manager_match_schedule.setOnClickListener {
 
             addFragment(SchedulePlayerScreen())
+        }
+    }
+
+    private fun toggleSection(bt: View, lyt: View) {
+        val show = toggleArrow(bt)
+        if (show) {
+            ViewAnimation.expand(lyt, object : ViewAnimation.AnimListener {
+                override fun onFinish() {
+                    Tools.nestedScrollTo(frg_manager_nested_scroll_view, lyt)
+                }
+            })
+        } else {
+            ViewAnimation.collapse(lyt)
+        }
+    }
+
+    private fun toggleArrow(view: View): Boolean {
+        return if (view.rotation == 0f) {
+            view.animate().setDuration(200).rotation(180f)
+            true
+        } else {
+            view.animate().setDuration(200).rotation(0f)
+            false
         }
     }
 }
---
+++ app/src/main/java/com/bongdaphui/utils/Tools.java
@@ -0,0 +1,16 @@
+package com.bongdaphui.utils;
+
+import android.support.v4.widget.NestedScrollView;
+import android.view.View;
+
+public class Tools {
+
+    public static void nestedScrollTo(final NestedScrollView nested, final View targetView) {
+        nested.post(new Runnable() {
+            @Override
+            public void run() {
+                nested.scrollTo(500, targetView.getBottom());
+            }
+        });
+    }
+}
